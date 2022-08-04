549a757a156fee4aac7f11dc2c99bda317334ec4
Spenser Black <spenserblack01@gmail.com>
2022-08-04 09:57:35 -0400

    Add prerelease input (#14)

--- __tests__/config.test.ts
+++ __tests__/config.test.ts
@@ -1,5 +1,5 @@
 import {describe, expect, test} from '@jest/globals'
-import {parseDraft} from '../src/config'
+import {parseDraft, parsePrerelease} from '../src/config'
 
 describe('config', () => {
   describe('parseDraft', () => {
@@ -16,6 +16,34 @@ describe('config', () => {
     test('throws invalid value', () => {
       process.env.INPUT_DRAFT = 'foo'
       expect(parseDraft).toThrow('Invalid draft value: foo')
+    })
+  })
+
+  describe('parsePrerelease', () => {
+    test.each(['true', 'True', 'TRUE', 'always'])(
+      '%s should be true',
+      input => {
+        process.env.INPUT_PRERELEASE = input
+        expect(parsePrerelease()).toBe(true)
+      }
+    )
+
+    test.each(['false', 'False', 'FALSE', 'never'])(
+      '%s should be false',
+      input => {
+        process.env.INPUT_PRERELEASE = input
+        expect(parsePrerelease()).toBe(false)
+      }
+    )
+
+    test('auto is auto', () => {
+      process.env.INPUT_PRERELEASE = 'auto'
+      expect(parsePrerelease()).toBe('auto')
+    })
+
+    test('throws invalid value', () => {
+      process.env.INPUT_PRERELEASE = 'foo'
+      expect(parsePrerelease).toThrow('Invalid prerelease value: foo')
     })
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import {setOutput, debug, notice} from '@actions/core'
 import {getOctokit} from '@actions/github'
 import Tag from './tag'
-import {parseDraft, parseDryRun, tag as rawTag, token} from './config'
+import {
+  parseDraft,
+  parseDryRun,
+  parsePrerelease,
+  prereleasePattern,
+  tag as rawTag,
+  token
+} from './config'
+import isPrerelease from './is-prerelease'
 
 async function run(): Promise<void> {
   debug(`Tag: ${rawTag}`)
@@ -21,6 +29,14 @@ async function run(): Promise<void> {
   debug(`Owner: ${owner}`)
   debug(`Repo: ${repo}`)
 
+  const prerelease = isPrerelease(parsePrerelease(), rawTag, prereleasePattern)
+
+  if (prerelease) {
+    debug('Detected prerelease')
+  }
+
+  setOutput('prerelease', prerelease)
+
   if (parseDryRun()) {
     notice('Dry run, skipping release creation', {title: 'Dry Run'})
     return
@@ -34,6 +50,7 @@ async function run(): Promise<void> {
     tag_name: rawTag,
     name,
     body,
+    prerelease,
     draft
   })
 }
